a06e0d2bfc004b7969ec25ecbd44650123d9ac97
Diane <dianedecourt@gmail.com>
2020-12-23 12:33:43 +0100

    update printf

--- ft_printf.c
+++ ft_printf.c
@@ -5,12 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/17 21:13:17 by ddecourt          #+#    #+#             */
-/*   Updated: 2020/12/19 13:34:38 by ddecourt         ###   ########.fr       */
+/*   Created: 2020/12/23 12:24:57 by ddecourt          #+#    #+#             */
+/*   Updated: 2020/12/23 12:28:37 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_printf(char *format, ...)
+void	ft_printf(const char *format, ...)
 {
+    va_list args;
+    int done;
 
+    va_start (args, format);
+    done = ft_vfprintf(stdout, format, args);
+    va_end (args);
+
+    return(done);
 }
+
+
