8e0d3f76592ec7dadb19a80d7d4c6fdbcfe56420
Diane DECOURT <ddecourt@e2r6p18.42.fr>
2021-04-03 14:25:20 +0200

    adding ft_strdup

---
+++ libft/ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/17 20:14:20 by ddecourt          #+#    #+#             */
+/*   Updated: 2021/04/02 15:11:14 by ddecourt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libftprintf.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(s1);
+	if (!(s2 = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (s1[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
