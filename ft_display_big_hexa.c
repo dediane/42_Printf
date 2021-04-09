143f1df821a995d0869b22502ccd52f0d33c15cf
dediane <dianedecourt@gmail.com>
2021-04-09 22:50:10 +0200

    normed on ft_display_big_hexa.c

--- ft_display_big_hexa.c
+++ ft_display_big_hexa.c
@@ -6,7 +6,7 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 22:46:57 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/04/09 21:48:26 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/04/09 22:49:02 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int			ft_display_big_hexa(unsigned int n, t_flags flags)
 	char	*res;
 	int		size;
 
-	if (flags.hash == 1 && n != 0)
-		ft_putstr("0X");
 	if (ft_check_exceptions_zero(n, flags) == 1)
 		return (0);
 	res = ft_convert(n, "0123456789ABCDEF");
@@ -28,10 +26,8 @@ int			ft_display_big_hexa(unsigned int n, t_flags flags)
 		ft_handle_prec_hexa(size, &flags);
 		ft_handle_width_hexa(size, &flags);
 		ft_putstr(res);
-		free(res);
-		return (0);
 	}
-	if ((flags.minus == 1) && (flags.zero == 0))
+	if (flags.minus == 1)
 	{
 		ft_handle_width_hexa(size, &flags);
 		if (flags.num_after > flags.num_before)
@@ -39,8 +35,6 @@ int			ft_display_big_hexa(unsigned int n, t_flags flags)
 		ft_putstr(res);
 		if (flags.num_after < flags.num_before)
 			ft_handle_prec_hexa(size, &flags);
-		free(res);
-		return (0);
 	}
 	free(res);
 	return (0);
