241ba420ed4a8741dc39badf9ecb1735ed2e11ee
dediane <dianedecourt@gmail.com>
2021-04-09 22:33:20 +0200

    normed file display_int.c

--- ft_display_int.c
+++ ft_display_int.c
@@ -6,16 +6,14 @@
 /*   By: ddecourt <ddecourt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 23:59:48 by ddecourt          #+#    #+#             */
-/*   Updated: 2021/04/09 21:58:50 by ddecourt         ###   ########.fr       */
+/*   Updated: 2021/04/09 22:32:27 by ddecourt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int				ft_handle_prec(int size, int nb, t_flags *flags)
+void			ft_handle_prec(int size, int nb, t_flags *flags)
 {
-	int tmp;
-
 	if ((flags->num_before != 0) && (flags->num_after == 0))
 	{
 		if ((flags->zero == 1) && (flags->dot == 0))
@@ -26,14 +24,13 @@ int				ft_handle_prec(int size, int nb, t_flags *flags)
 	if ((flags->num_before != 0) && (flags->num_after != 0))
 	{
 		if ((flags->num_after < flags->num_before) && (flags->num_after < size))
-			 ft_write_width(' ', flags->num_before, size);
+			ft_write_width(' ', flags->num_before, size);
 		if ((flags->num_after < flags->num_before) && (flags->num_after > size))
 		{
 			if (nb < 0)
-				tmp = flags->num_after + 1;
+				ft_write_width(' ', flags->num_before, flags->num_after + 1);
 			else
-				tmp = flags->num_after;
-			ft_write_width(' ', flags->num_before, tmp);
+				ft_write_width(' ', flags->num_before, flags->num_after);
 		}
 		if (flags->num_after > flags->num_before)
 		{
@@ -42,7 +39,6 @@ int				ft_handle_prec(int size, int nb, t_flags *flags)
 			ft_write_width('0', flags->num_after, size);
 		}
 	}
-	return(0);
 }
 
 int				ft_handle_width(int size, int nb, t_flags *flags)
@@ -60,20 +56,40 @@ int				ft_handle_width(int size, int nb, t_flags *flags)
 		if (flags->num_after > flags->num_before)
 			ft_write_width('0', flags->num_before, size);
 	}
-	return(0);
+	return (0);
 }
 
-
-int				ft_display_int(int n, t_flags flags)
+int				ft_display_prec_width(int nb, int size, t_flags flags)
 {
-	long int nb;
-	int			size;
+	if ((flags.minus == 1) || ((flags.num_after > flags.num_before)
+				&& (nb < 0)))
+	{
+		if (nb < 0)
+			ft_putchar('-');
+		ft_handle_width(size, nb, &flags);
+		if (flags.num_after > flags.num_before)
+			ft_handle_prec(size, nb, &flags);
+		ft_putnbr(nb);
+		if (flags.num_after < flags.num_before)
+			ft_handle_prec(size, nb, &flags);
+		return (0);
+	}
+	if (flags.minus == 0)
+	{
+		ft_handle_prec(size, nb, &flags);
+		if (flags.num_after > flags.num_before)
+			ft_handle_width(size, nb, &flags);
+		if (nb < 0)
+			ft_putchar('-');
+		if (flags.num_after < flags.num_before)
+			ft_handle_width(size, nb, &flags);
+		ft_putnbr(nb);
+	}
+	return (0);
+}
 
-	nb = (long int)n;
-	size = ft_get_size(nb);
-
-	if (ft_check_exceptions_zero(nb, flags) == 1)
-		return(0);
+void			ft_display_prec(int nb, int size, t_flags flags)
+{
 	if ((flags.num_before == 0) && (flags.num_after == 0))
 	{
 		if (nb < 0)
@@ -92,6 +108,21 @@ int				ft_display_int(int n, t_flags flags)
 		if (flags.minus == 1)
 			ft_handle_prec(size, nb, &flags);
 	}
+}
+
+int				ft_display_int(int n, t_flags flags)
+{
+	long int	nb;
+	int			size;
+
+	nb = (long int)n;
+	size = ft_get_size(nb);
+	if (ft_check_exceptions_zero(nb, flags) == 1)
+		return (0);
+	if ((flags.num_before == 0) && (flags.num_after == 0))
+		ft_display_prec(nb, size, flags);
+	if ((flags.num_before != 0) && (flags.num_after == 0))
+		ft_display_prec(nb, size, flags);
 	if ((flags.num_before == 0) && (flags.num_after != 0))
 	{
 		if (nb < 0)
@@ -103,32 +134,6 @@ int				ft_display_int(int n, t_flags flags)
 		ft_putnbr(nb);
 	}
 	if ((flags.num_before != 0) && (flags.num_after != 0))
-	{
-		if ((flags.minus == 1) || ((flags.num_after > flags.num_before) && (nb < 0)))
-		{
-			if (nb < 0)
-			{
-				ft_putchar('-');
-			}
-			ft_handle_width(size, nb, &flags);
-			if (flags.num_after > flags.num_before)
-				ft_handle_prec(size, nb, &flags);
-			ft_putnbr(nb);
-			if (flags.num_after < flags.num_before)
-				ft_handle_prec(size, nb, &flags);
-			return (0);
-		}
-		if (flags.minus == 0)
-		{	
-			ft_handle_prec(size, nb, &flags);
-			if (flags.num_after > flags.num_before)
-				ft_handle_width(size, nb, &flags);
-			if (nb < 0)
-				ft_putchar('-');
-			if (flags.num_after < flags.num_before)
-				ft_handle_width(size, nb, &flags);
-			ft_putnbr(nb);
-		}
-	}
-	return(0);
+		ft_display_prec_width(nb, size, flags);
+	return (0);
 }
